c69ab7c3ac4527624e7d2ccfe2fb2bccc6d07c79
nam31804 <nam31804@gmail.com>
2024-04-17 23:05:47 +0700

    bai2

---
+++ Lab02_2.NQN/Controllers/NQN.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab02_2.NQN.Controllers
+{
+    public class NQNController : Controller
+    {
+        // GET: NQN
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ShowProduct()
+        {
+            return View();
+        }
+        public ActionResult EditProduct(int? productId)
+        {
+            ViewBag.id = productId;
+            return View();
+        }
+        public ActionResult DetailsProduct(string productName, int? productId)
+        {
+            ViewBag.name = productName;
+            ViewBag.id = productId;
+            return View();
+        }
+    }
+}
